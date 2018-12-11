1d771b76a7962f9910d051025235e9eec744ce2d
lvovard <vovardlaurent@gmail.com>
2018-12-11 21:21:53 +0100

    essai1

--- src/com/lvovard/sportteam/AdminCatActivity.java
+++ src/com/lvovard/sportteam/AdminCatActivity.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
+//test
 public class AdminCatActivity extends Activity
 {
   protected Button btnAddCat;
